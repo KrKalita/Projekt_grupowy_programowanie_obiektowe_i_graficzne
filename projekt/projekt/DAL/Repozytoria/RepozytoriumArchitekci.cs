ea6765bed298ff9fcd4877e8a114a7d606b5bed1
Marta Kirsch <martakirsch00@gmail.com>
2021-06-24 09:44:59 +0200

    stworzenie folderu projektu, utworzenie odpowiedniego dla MVVM ukladu folderow oraz dodanie pierwszych klas odpowiedzialnych za polaczenie z bd

---
+++ projekt/projekt/DAL/Repozytoria/RepozytoriumArchitekci.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt.DAL.Repozytoria
+{
+    using Encje;
+    using MySqlConnector;
+
+    static class RepozytoriumArchitekci
+    {
+        #region ZAPYTANIA
+        private const string WSZYSCY_ARCHITEKCI = "SELECT * FROM architekci";
+        #endregion
+
+
+        public static List<Architekt> PobierzWszystkichArchitektow()
+        {
+            List<Architekt> architekci = new List<Architekt>();
+
+            using (var connection = DBConnection.Instance.Connection)
+            {
+                MySqlCommand command = new MySqlCommand(WSZYSCY_ARCHITEKCI, connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                    architekci.Add(new Architekt(reader));
+                connection.Close();
+            }
+            return architekci;
+        }
+    }
+}
